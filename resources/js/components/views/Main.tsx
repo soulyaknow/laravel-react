5f156946c4a79afd058d3cc230b69269f015d645
SoulYaKnow <ranel.soliano@gmail.com>
2024-07-10 14:50:28 +0800

    first commit

---
+++ resources/js/components/views/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navar from "../navar/Navar";
+const Main = () => {
+    return (
+        <div className="flex flex-col min-h-max">
+            <Navar />
+            <div>dasdasd</div>
+        </div>
+    );
+};
+
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.ts",
+    "./resources/**/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["luxury"],
+  },
+}
+
